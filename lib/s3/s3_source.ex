3b6989f5963802861dfc3a8e2821ad2fd469ab92
Radosław Szuma <radoslawszuma@gmail.com>
2024-02-28 13:58:56 +0100

    Add Membrane.AWS.S3.Source

---
+++ lib/s3/s3_source.ex
@@ -0,0 +1,65 @@
+defmodule Membrane.AWS.S3.Source do
+  @moduledoc """
+  Element that reads file from a S3 bucket and sends it through the output pad.
+  """
+  use Membrane.Source
+
+  alias Membrane.{Buffer, RemoteStream}
+
+  def_options aws_credentials: [
+                spec: Keyword.t(),
+                description: "Credentials to AWS",
+                default: []
+              ],
+              bucket: [
+                spec: binary(),
+                description: "Name of bucket"
+              ],
+              path: [
+                spec: binary(),
+                description: "Path to file in bucket"
+              ],
+              opts: [
+                spec: [
+                  max_concurrency: pos_integer(),
+                  chunk_size: pos_integer(),
+                  timeout: pos_integer()
+                ],
+                description: "File download opts",
+                default: []
+              ]
+
+  def_output_pad :output, accepted_format: %RemoteStream{type: :bytestream}, flow_control: :manual
+
+  @impl true
+  def handle_init(_context, opts) do
+    {[], Map.from_struct(opts)}
+  end
+
+  @impl true
+  def handle_playing(_ctx, state) do
+    ExAws
+
+    file_stream =
+      state.bucket
+      |> ExAws.S3.download_file(state.path, :memory, state.opts)
+      |> ExAws.stream!(state.aws_credentials)
+
+    # Uncomment if you need to gunzip (and add dependency :stream_gzip)
+    # |> StreamGzip.gunzip()
+
+    {[stream_format: {:output, %RemoteStream{type: :bytestream}}], file_stream}
+  end
+
+  @impl true
+  def handle_demand(_pad, _size, _unit, _ctx, file_stream) do
+    case Enum.take(file_stream, 1) do
+      [] ->
+        {[end_of_stream: :output], file_stream}
+
+      [payload] ->
+        file_stream = Stream.drop(file_stream, 1)
+        {[buffer: {:output, %Buffer{payload: payload}}] ++ [redemand: :output], file_stream}
+    end
+  end
+end
---
+++ test/s3_source_test.exs
@@ -0,0 +1,124 @@
+defmodule Membrane.AWS.S3.SourceTest do
+  use ExUnit.Case, async: true
+
+  import Membrane.Testing.Assertions
+  import Membrane.ChildrenSpec
+  import Support.BypassHelpers
+
+  alias Membrane.AWS.S3.Source
+  alias Membrane.Testing.{Pipeline, Sink}
+
+  describe "file pass through pipeline" do
+    setup [:start_bypass]
+
+    test "whole file in one chunk", %{bypass: bypass} do
+      data = for i <- 0..10_000, do: <<i::8>>, into: <<>>
+
+      setup_multipart_download_backend(
+        bypass,
+        "bucket",
+        "test.txt",
+        data
+      )
+
+      assert pipeline =
+               Pipeline.start_link_supervised!(
+                 spec:
+                   child(:s3_source, %Source{
+                     bucket: "bucket",
+                     path: "test.txt",
+                     aws_credentials: exaws_config_for_bypass(bypass)
+                   })
+                   |> child(:sink, Sink),
+                 test_process: self()
+               )
+
+      assert_sink_buffer(
+        pipeline,
+        :sink,
+        %Membrane.Buffer{
+          payload: ^data
+        }
+      )
+
+      assert_end_of_stream(pipeline, :sink)
+    end
+
+    test "file splitted to 16-bytes chunks", %{bypass: bypass} do
+      chunk_size = 128
+
+      data = for i <- 0..10_000, do: <<i::8>>, into: <<>>
+
+      splitted_binary =
+        for <<chunk::binary-size(chunk_size) <- data>>, do: <<chunk::binary-size(chunk_size)>>
+
+      setup_multipart_download_backend(
+        bypass,
+        "bucket",
+        "test2.txt",
+        data
+      )
+
+      assert pipeline =
+               Pipeline.start_link_supervised!(
+                 spec:
+                   child(:s3_source, %Source{
+                     bucket: "bucket",
+                     path: "test2.txt",
+                     aws_credentials: exaws_config_for_bypass(bypass),
+                     opts: [chunk_size: chunk_size]
+                   })
+                   |> child(:sink, Sink),
+                 test_process: self()
+               )
+
+      Enum.each(splitted_binary, fn expected_payload ->
+        assert_sink_buffer(
+          pipeline,
+          :sink,
+          %Membrane.Buffer{
+            payload: ^expected_payload
+          }
+        )
+      end)
+
+      assert_end_of_stream(pipeline, :sink)
+    end
+  end
+
+  defp setup_multipart_download_backend(
+         bypass,
+         bucket_name,
+         path,
+         file_body
+       ) do
+    request_path = "/#{bucket_name}/#{path}"
+
+    Bypass.expect(bypass, fn conn ->
+      case conn do
+        %{method: "HEAD", request_path: ^request_path} ->
+          conn
+          |> Plug.Conn.put_resp_header("Content-Length", file_body |> byte_size |> to_string)
+          |> Plug.Conn.send_resp(200, "")
+
+        %{method: "GET", request_path: ^request_path, req_headers: headers} ->
+          {_key, "bytes=" <> range} = Enum.find(headers, fn {key, _val} -> key == "range" end)
+
+          [first, second | _] = String.split(range, "-")
+
+          first = String.to_integer(first)
+          second = String.to_integer(second)
+
+          # IO.inspect({first, second, second - first + 1}, label: :difference)
+
+          <<_head::binary-size(first), payload::binary-size(second - first + 1), _rest::binary>> =
+            file_body
+
+          # IO.inspect(byte_size(payload), label: :WTF_PAYLOAD)
+
+          conn
+          |> Plug.Conn.send_resp(200, payload)
+      end
+    end)
+  end
+end
